4a8c4da947bfe968466abb8c74df6881dc01c1b8
Stanley Jia <stanleyjia101@gmail.com>
2021-01-31 17:01:20 -0800

    added callback function

--- auth-app/src/Components/MoodData.js
+++ auth-app/src/Components/MoodData.js
@@ -3,39 +3,40 @@ import '../App.css';
 import { database } from '../Firebase.js';
 import { date } from './GetDate.js';
 
-async function getMoodLog() {
-    //Haven't updated for change of data structure yet
-    //const userInfo = auth.currentUser;
-    //const uid = userInfo.uid;
-    const uid = 'BQJQmkLEsmSLFn6WF2JWWlXZzVo1';
-    var moods = {};
-    var moodObj = {};
+async function getMoodLog(callback) {
+  //Haven't updated for change of data structure yet
+  //const userInfo = auth.currentUser;
+  //const uid = userInfo.uid;
+  const uid = 'BQJQmkLEsmSLFn6WF2JWWlXZzVo1';
+  var moods = [];
+  var moodObj = {};
 
-    const timeToInteger = (time) => {
-        var t = time.split(':');
-        for (var i=0; i<t.length; i++){
-        t[i] = parseInt(t[i]);
-        }
-        const timeAsInt = ((t[0]*3600) + (t[1]*60) + t[2]);
-        return (timeAsInt);
+  const timeToInteger = (time) => {
+    var t = time.split(':');
+    for (var i = 0; i < t.length; i++) {
+      t[i] = parseInt(t[i]);
     }
+    const timeAsInt = ((t[0] * 3600) + (t[1] * 60) + t[2]);
+    return (timeAsInt);
+  }
 
-    const ref = database.ref('moods/'+uid+'/'+date);
-    ref.once('value', (snapshot) => {
-        snapshot.forEach((a) => {
-            const t = a.key;
-            const time = timeToInteger(t);
-            const m = a.val();
-            const mood = m[Object.keys(m)[0]];
+  const ref = database.ref('moods/' + uid + '/' + date);
+  ref.once('value', (snapshot) => {
+    var moods = [];
+    snapshot.forEach((a) => {
+      const t = a.key;
+      const time = timeToInteger(t);
+      const m = a.val();
+      const mood = m[Object.keys(m)[0]];
 
-            moodObj = {
-                'time' : time,
-                'mood' : mood          
-            }
-            moods [ time ] =  mood ;
-        })
-    })
-    return(moods);
+      var moodObj = {
+        'time': time,
+        'mood': mood
+      };
+      moods.push(moodObj);
+    });
+    callback(moods);
+  });
 }
 
 export default getMoodLog;
--- auth-app/src/Components/WebsiteByMood.js
+++ auth-app/src/Components/WebsiteByMood.js
@@ -2,67 +2,56 @@ import React, { Component } from 'react';
 import getMoodLog from './MoodData.js';
 import { webLog } from './WebData.js';
 
-// async function getMood(){
-//     const moodLog = await getMoodLog().then(result => result.data);
-//     return moodLog;
-// }
-
-
-
 const hourBeforeArray = (time) => {
-    var hba = [];
-    for (var i=0; i<webLog.length; i++){
-      var start = webLog[i].start;
-      var end = webLog[i].end;
-      //console.log(start);
-      if (start > (time-3600) && start < time){
-        console.log('found')
-        var timeDiff = end - start;
-        const a = {
-            'domain' : webLog[i].domain,
-            'timeDiff' : timeDiff
-        }
-        hba.push(a);
-        console.log(a);
+  var hba = [];
+  for (var i = 0; i < webLog.length; i++) {
+    var start = webLog[i].start;
+    var end = webLog[i].end;
+    //console.log(start);
+    if (start > (time - 3600) && start < time) {
+      console.log('found')
+      var timeDiff = end - start;
+      const a = {
+        'domain': webLog[i].domain,
+        'timeDiff': timeDiff
       }
+      hba.push(a);
+      console.log(a);
     }
-    return (hba);
-}
-function compare (a, b){
-    const timeA = a.time;
-    const timeB = b.time;
-
-    let comparison = 0;
-    if (timeA > timeB) {
-      comparison = -1;
-    }
-    else if (timeA < timeB){
-      comparison = 1;
-    }
-    return comparison;
   }
+  return (hba);
+};
+function compare(a, b) {
+  const timeA = a.time;
+  const timeB = b.time;
 
-const anxious = async () => {
-    const moodLog = await getMoodLog();
-    //console.log(webLog)
-    var timeByDomain = [];
-    //console.log(Object.keys(moodLog));
-    console.log(moodLog)
-    //console.log(typeof(moodLog));
-    //starting here
+  let comparison = 0;
+  if (timeA > timeB) {
+    comparison = -1;
+  }
+  else if (timeA < timeB) {
+    comparison = 1;
+  }
+  return comparison;
+}
 
-    // Object.keys(moodLog).forEach(function(key){
-    //     console.log(key.moodLog[key]);
-    // });
-    for (let [key, value] of Object.entries(moodLog)){
-        console.log(`${key}:${value}`);
-    }
-    // for (var key in moodLog){
-    //     console.log(key);
-    // }
-    //     console.log(moodLog[i]);
-    //     if (moodLog[i].mood === 'Anxious'){
-    //         var time = moodLog[i].time;
+var getAnxious = async () => {
+  await getMoodLog(anxiousRetrieved);
+};
+
+function anxiousRetrieved(moodLog) {
+  // var timeByDomain = [];
+  // console.log(typeof (moodLog));
+  // console.log(`here ${moodLog}`);
+  // console.log(moodLog.keys());
+
+  console.log(moodLog);
+  // console.log(Object.entries(moodLog));
+  for (var key in moodLog) {
+    console.log(moodLog[key]);
+    //     console.log(moodLog[key]);
+    //     if (moodLog[key].mood === 'Anxious'){
+    //         var time = moodLog[key].time;
     //         // add conditional loop for if time < 3600
     //         // x is the array of the web times for the hour leading up to the mood log
     //         const x  = hourBeforeArray(time)
@@ -92,23 +81,25 @@ const anxious = async () => {
     //     {name: topFive[1].domain, seconds: topFive[1].time},
     //     {name: topFive[2].domain, seconds: topFive[2].time},
     //     {name: topFive[3].domain, seconds: topFive[3].time},
-    //     {name: topFive[4].domain, seconds: topFive[4].time},        
+    //     {name: topFive[4].domain, seconds: topFive[4].time},
     // ]
     //return(anxiousData);
-    return([]);
-
-
-
-    //ending here
-    // const anxiousData = [
-    //     {name: timeByDomain[0].domain, seconds: timeByDomain[0].time},
-    //     {name: timeByDomain[1].domain, seconds: timeByDomain[1].time},
-    //     {name: timeByDomain[2].domain, seconds: timeByDomain[2].time},
-    //     {name: timeByDomain[3].domain, seconds: timeByDomain[3].time},
-    //     {name: timeByDomain[4].domain, seconds: timeByDomain[4].time},        
-    // ]
+  }
+  return ([]);
 }
+
+
+
+//ending here
+// const anxiousData = [
+//     {name: timeByDomain[0].domain, seconds: timeByDomain[0].time},
+//     {name: timeByDomain[1].domain, seconds: timeByDomain[1].time},
+//     {name: timeByDomain[2].domain, seconds: timeByDomain[2].time},
+//     {name: timeByDomain[3].domain, seconds: timeByDomain[3].time},
+//     {name: timeByDomain[4].domain, seconds: timeByDomain[4].time},
+// ]
+// }
 // export default tim;
 
 // // console.log(timeByDomain);
-export const timeByDom = anxious();
+export const timeByDom = getAnxious();
