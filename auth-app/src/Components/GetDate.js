a07044fbd7111432888e9a27c4e7d6005dee1fa3
Stanley Jia <stanleyjia101@gmail.com>
2021-02-03 22:07:42 -0800

    removed auth-app

--- auth-app/src/Components/GetDate.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const today = new Date();
-const month = today.getMonth() + 1;
-const day = today.getDate();
-const year = today.getFullYear();
-export const date = month+'-'+day+'-'+year;
