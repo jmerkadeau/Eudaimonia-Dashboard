8016fac8f21c5567dc8700c6d0b62a5247394a2c
Stanley Jia <stanleyjia101@gmail.com>
2021-03-03 22:07:11 -0800

    merged

--- app/src/Components/LandingPage/Sections/OurServices.js
+++ app/src/Components/LandingPage/Sections/OurServices.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Icon, Button, useMediaQuery, Container, Typography, ThemeProvider, createMuiTheme, makeStyles } from "@material-ui/core";
 import stockTyping from './../../../stockTyping.jpg';
+import webByMood from "./../../../img/moodByWeb.png";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './Theme.js';
 
@@ -14,65 +15,65 @@ import theme from './Theme.js';
 // });
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        marginTop: theme.spacing(10),
-        marginBottom: theme.spacing(15)
-    },
-    rightText: {
-        marginRight: theme.spacing(20),
-    },
-    photo: {
-      marginTop: theme.spacing(12),
-      marginLeft: theme.spacing(15),
-      width: '80%'
-    },
-    title: {
-      textAlign: 'center',
-      marginRight: theme.spacing(25)
-    }
+  root: {
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(15)
+  },
+  rightText: {
+    marginRight: theme.spacing(20),
+  },
+  photo: {
+    marginTop: theme.spacing(12),
+    marginLeft: theme.spacing(15),
+    width: '80%'
+  },
+  title: {
+    textAlign: 'center',
+    marginRight: theme.spacing(25)
+  }
 }))
 
 const Services = () => {
-    const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
-    const classes = useStyles();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+  const classes = useStyles();
 
 
-    return(
-        <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <section className={classes.root}>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <section className={classes.root}>
         <div className="container">
           <Grid container spacing={isMobile ? 4 : 10} justify="space-between">
             <Grid item sm={6} xs={12} >
               <img
-                src={stockTyping}
+                src={webByMood}
                 alt="graduate"
                 className={classes.photo}
-                // className="max-h-500 max-w-full border-radius-12"
+              // className="max-h-500 max-w-full border-radius-12"
               />
             </Grid>
             <Grid item sm={6} xs={12}>
-                <Typography variant='h2' color='primary' className={classes.title}>
-                    Our Mission
-                </Typography><br/>
-                <Typography variant='h5' color='primary'>
-                  The Problem
+              <Typography variant='h2' color='primary' className={classes.title}>
+                Our Mission
+                </Typography><br />
+              <Typography variant='h5' color='primary'>
+                The Problem
                 </Typography>
-                <Typography className={classes.rightText}>
-                
+              <Typography className={classes.rightText}>
 
-                Studies have consistently shown that excessive web consumption leads 
-                to increased feelings of anxiety, isolation, and depression. <br/><br/> However, 
-                there is a lack of substantial research on how specific sites influence 
-                your mental health.<br/><hr/>
+
+                Studies have consistently shown that excessive web consumption leads
+                to increased feelings of anxiety, isolation, and depression. <br /><br /> However,
+                there is a lack of substantial research on how specific sites influence
+                your mental health.<br /><hr />
+              </Typography>
+              <Typography variant='h5' color='primary'>
+                Our Solution
                 </Typography>
-                <Typography variant='h5' color='primary'>
-                  Our Solution
-                </Typography>
-                <Typography className={classes.rightText}>
-                With data privacy in mind, we’ve developed a secure cloud platform enabling you to better understand how 
-                web usage patterns affect your own mood throughout the day. <br/><br/> By sharing our technology and 
-                our vision, we empower you to improve your own mental health through key 
+              <Typography className={classes.rightText}>
+                With data privacy in mind, we’ve developed a secure cloud platform enabling you to better understand how
+                web usage patterns affect your own mood throughout the day. <br /><br /> By sharing our technology and
+                our vision, we empower you to improve your own mental health through key
                 insights and the adoption of healthier online habits.
                 </Typography>
 
@@ -81,41 +82,41 @@ const Services = () => {
                 Our Services
               </h1>
               <p className="my-8 max-w-400">
-              Project Eudaimonia is our solution. With data privacy in mind, 
-              we’ve developed a secure cloud platform to help users better 
-              understand how the internet affects their mental health. We combine 
-              the functionality of productivity trackers like RescueTime with a 
-              simple and clean user interface for mood logging. 
-  
+              Project Eudaimonia is our solution. With data privacy in mind,
+              we’ve developed a secure cloud platform to help users better
+              understand how the internet affects their mental health. We combine
+              the functionality of productivity trackers like RescueTime with a
+              simple and clean user interface for mood logging.
+
               </p> */}
             </Grid>
           </Grid>
         </div>
       </section>
-      </ThemeProvider>
-        
-        // <section className='section'>
-        //     <div className='container'>
-        //     <Grid container>
-        //     <Grid>
-        //         <img src={stockTyping} alt="Typing"/>
-        //     </Grid>
-        //     <Grid>
-                // <Typography variant='h2' color='primary'>
-                //     Our Services
-                // </Typography>
-                // <Typography>
-                //     Project Eudaimonia is our solution. With data privacy in mind, 
-                //     we’ve developed a secure cloud platform to help users better 
-                //     understand how the internet affects their mental health. We combine 
-                //     the functionality of productivity trackers like RescueTime with a 
-                //     simple and clean user interface for mood logging. 
-                // </Typography>
-        //     </Grid>
-        //     </Grid>
-        //     </div>
-        // </section>
-    )
+    </ThemeProvider>
+
+    // <section className='section'>
+    //     <div className='container'>
+    //     <Grid container>
+    //     <Grid>
+    //         <img src={stockTyping} alt="Typing"/>
+    //     </Grid>
+    //     <Grid>
+    // <Typography variant='h2' color='primary'>
+    //     Our Services
+    // </Typography>
+    // <Typography>
+    //     Project Eudaimonia is our solution. With data privacy in mind,
+    //     we’ve developed a secure cloud platform to help users better
+    //     understand how the internet affects their mental health. We combine
+    //     the functionality of productivity trackers like RescueTime with a
+    //     simple and clean user interface for mood logging.
+    // </Typography>
+    //     </Grid>
+    //     </Grid>
+    //     </div>
+    // </section>
+  )
 }
 
 export default Services;
--- app/src/Components/LandingPage/Sections/OurValues.js
+++ app/src/Components/LandingPage/Sections/OurValues.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Grid, Icon, Button, useMediaQuery, Container, Typography, ThemeProvider, createMuiTheme, makeStyles, Fab } from "@material-ui/core";
 import stockTyping from './../../../stockTyping.jpg';
+// import webByMood from "./../../../img/moodLogger.png";
+
 import { Link as RouterLink } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './Theme.js';
@@ -25,142 +27,141 @@ import theme from './Theme.js';
 // });
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        marginTop: theme.spacing(10),
-        marginBottom: theme.spacing(15)
-    },
-    leftText: {
-        marginLeft: theme.spacing(20),
-        paddingRight: theme.spacing(5),
-        color: '#616161'
-    },
-    leftSubHeaders: {
-        marginLeft: theme.spacing(20),
-        color: '#999999'
-    },
-    photo: {
-      marginTop: theme.spacing(15),
-      marginRight: theme.spacing(15),
-      width: '80%'
-    },
-    title: {
-      textAlign: 'center',
-      marginLeft: theme.spacing(15)
-    },
-    leftSide: {
-        
+  root: {
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(15)
+  },
+  leftText: {
+    marginLeft: theme.spacing(20),
+    paddingRight: theme.spacing(5),
+    color: '#616161'
+  },
+  leftSubHeaders: {
+    marginLeft: theme.spacing(20),
+    color: '#999999'
+  },
+  photo: {
+    marginTop: theme.spacing(15),
+    marginRight: theme.spacing(15),
+    width: '80%'
+  },
+  title: {
+    textAlign: 'center',
+    marginLeft: theme.spacing(15)
+  },
+  leftSide: {
 
-    },
-    privacyButton: {
-        display: 'flex',
-        justifyContent: 'center',
-        marginLeft: theme.spacing(10),
-        marginTop: theme.spacing(2),
-    },
-    privacyFab: {
-      backgroundColor: '#999999',
-      color: 'white',
-      '&:hover': {
-        backgroundColor: '#616161',
+
+  },
+  privacyButton: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: theme.spacing(10),
+    marginTop: theme.spacing(2),
+  },
+  privacyFab: {
+    backgroundColor: '#999999',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#616161',
     }
-    },
+  },
 }))
 
 const OurValues = () => {
-    const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
-    const classes = useStyles();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+  const classes = useStyles();
 
 
-    return(
-        <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <section className={classes.root}>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <section className={classes.root}>
         <div className="container">
           <Grid container spacing={isMobile ? 4 : 10} justify="space-between">
             <Grid item sm={6} xs={12} className={classes.leftSide}>
-                <Typography variant='h2' color='primary' className={classes.title}>
-                    Our Values
-                </Typography><br/>
-                <Typography variant='h5' color='primary' className={classes.leftSubHeaders}>
-                  Ease of use
+              <Typography variant='h2' color='primary' className={classes.title}>
+                Our Values
+                </Typography><br />
+              <Typography variant='h5' color='primary' className={classes.leftSubHeaders}>
+                Ease of use
                 </Typography>
-                <Typography className={classes.leftText}>
-                    Simple is always better. Through our minimalist design and carefully 
-                    curated feature set, we strive to make it as easy as possible 
-                    to use our tools.
-                </Typography><br/>
-                <Typography variant='h5' color='primary' className={classes.leftSubHeaders}>
-                  Empowerment
+              <Typography className={classes.leftText}>
+                Simple is always better. Through our minimalist design and carefully
+                curated feature set, we strive to make it as easy as possible
+                to use our tools.
+                </Typography><br />
+              <Typography variant='h5' color='primary' className={classes.leftSubHeaders}>
+                Empowerment
                 </Typography>
-                <Typography className={classes.leftText}>
-                Our goal isn’t to force change, but rather to empower you to find your 
+              <Typography className={classes.leftText}>
+                Our goal isn’t to force change, but rather to empower you to find your
                 own path to wellness and happiness.
-                </Typography><br/>
-                <Typography variant='h5' color='primary' className={classes.leftSubHeaders}>
-                  Integrity
+                </Typography><br />
+              <Typography variant='h5' color='primary' className={classes.leftSubHeaders}>
+                Integrity
                 </Typography>
-                <Typography className={classes.leftText}>
-                    We operate with full transparency and are accountable by measuring our actions 
-                    against the highest standards of integrity and responsibility to the community.
-                </Typography><br/>
-                <Typography className={classes.leftText}>
-                    Moreover, it is fundamental to our values to protect user data and privacy. 
-                    View our full privacy policy here:
+              <Typography className={classes.leftText}>
+                We operate with full transparency and are accountable by measuring our actions
+                against the highest standards of integrity and responsibility to the community.
+                </Typography><br />
+              <Typography className={classes.leftText}>
+                Moreover, it is fundamental to our values to protect user data and privacy.
+                View our full privacy policy here:
                 </Typography>
-                <div className={classes.privacyButton}>
-                <RouterLink style={{ textDecoration: 'none'}} to="/privacy">
-                    <Fab className={classes.privacyFab} variant="extended">
+              <div className={classes.privacyButton}>
+                <RouterLink style={{ textDecoration: 'none' }} to="/privacy">
+                  <Fab className={classes.privacyFab} variant="extended">
                     Privacy Policy
                     </Fab>
                 </RouterLink>
-                </div>
+              </div>
             </Grid>
             <Grid item sm={6} xs={12}>
-            <img
+              <img
                 src={stockTyping}
                 alt="graduate"
                 className={classes.photo}
-                // className="max-h-500 max-w-full border-radius-12"
               />
               {/* <h1 className="mt-0 text-48 font-normal mb-8 inline-block">
                 Our Services
               </h1>
               <p className="my-8 max-w-400">
-              Project Eudaimonia is our solution. With data privacy in mind, 
-              we’ve developed a secure cloud platform to help users better 
-              understand how the internet affects their mental health. We combine 
-              the functionality of productivity trackers like RescueTime with a 
-              simple and clean user interface for mood logging. 
-  
+              Project Eudaimonia is our solution. With data privacy in mind,
+              we’ve developed a secure cloud platform to help users better
+              understand how the internet affects their mental health. We combine
+              the functionality of productivity trackers like RescueTime with a
+              simple and clean user interface for mood logging.
+
               </p> */}
             </Grid>
           </Grid>
         </div>
       </section>
-      </ThemeProvider>
-        
-        // <section className='section'>
-        //     <div className='container'>
-        //     <Grid container>
-        //     <Grid>
-        //         <img src={stockTyping} alt="Typing"/>
-        //     </Grid>
-        //     <Grid>
-                // <Typography variant='h2' color='primary'>
-                //     Our Services
-                // </Typography>
-                // <Typography>
-                //     Project Eudaimonia is our solution. With data privacy in mind, 
-                //     we’ve developed a secure cloud platform to help users better 
-                //     understand how the internet affects their mental health. We combine 
-                //     the functionality of productivity trackers like RescueTime with a 
-                //     simple and clean user interface for mood logging. 
-                // </Typography>
-        //     </Grid>
-        //     </Grid>
-        //     </div>
-        // </section>
-    )
+    </ThemeProvider>
+
+    // <section className='section'>
+    //     <div className='container'>
+    //     <Grid container>
+    //     <Grid>
+    //         <img src={stockTyping} alt="Typing"/>
+    //     </Grid>
+    //     <Grid>
+    // <Typography variant='h2' color='primary'>
+    //     Our Services
+    // </Typography>
+    // <Typography>
+    //     Project Eudaimonia is our solution. With data privacy in mind,
+    //     we’ve developed a secure cloud platform to help users better
+    //     understand how the internet affects their mental health. We combine
+    //     the functionality of productivity trackers like RescueTime with a
+    //     simple and clean user interface for mood logging.
+    // </Typography>
+    //     </Grid>
+    //     </Grid>
+    //     </div>
+    // </section>
+  )
 }
 
 export default OurValues;
